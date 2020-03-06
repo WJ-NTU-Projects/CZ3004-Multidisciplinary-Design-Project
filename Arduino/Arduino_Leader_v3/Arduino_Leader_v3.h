a2aa1f7d3b3d83d2f067f349b64f4b8f8346faf2
Teo Wei Jie <wjayteo@gmail.com>
2020-03-06 14:29:17 +0800

    Update Arduino_Leader_v3.h

--- Arduino/Arduino_Leader_v3/Arduino_Leader_v3.h
+++ Arduino/Arduino_Leader_v3/Arduino_Leader_v3.h
@@ -13,9 +13,9 @@
 
 // 2.89
 // 4.591
-#define TICKS_PER_MM 2.96
+#define TICKS_PER_MM_FAST 2.96
 #define TICKS_MULTIPLIER 0.41
-//#define TICKS_PER_MM 2.92
+#define TICKS_PER_MM 2.90
 #define TICKS_PER_ANGLE 4.58
 
 volatile boolean movingLeft = false;
@@ -31,6 +31,7 @@ double speedLeft = 0;
 double speedRight = 0;
 double error = 0;
 double setpoint = 0;
+int moved = 0;
 
 Motor motor;
 Sensors sensors;
